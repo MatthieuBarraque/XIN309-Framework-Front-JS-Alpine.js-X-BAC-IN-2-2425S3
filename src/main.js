d3016271bd4e9dbcb9ae6e191afc122ec6d826e8
matthieu <matthieu.barraque@outlook.fr>
2024-11-24 13:12:06 +0100

    [ADD] -> login firebase

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import Navbar from './components/Navbar/Navbar.js';
-import { setupRouter } from './routes/router.js';
-
-// Insérer la navbar dans le DOM
-document.getElementById('app').innerHTML = Navbar();
-
-// Configurer le système de routage
-setupRouter();
